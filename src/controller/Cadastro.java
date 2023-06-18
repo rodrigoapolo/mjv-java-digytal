9a9280e6588bba1e7c38dda314bfee01f3d00141
rodrigoapolo <rodrigoapolodev@gmail.com>
2023-06-18 00:48:45 -0300

    adicionar regra no metodo

---
+++ src/controller/Cadastro.java
@@ -0,0 +1,186 @@
+package controller;
+
+import exception.CadastroException;
+import model.Habilidade;
+import model.Pesssoa;
+import model.SEXO;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Cadastro {
+    private Pesssoa pesssoa = new Pesssoa();
+    private DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Pesssoa getPesssoa() {
+        return pesssoa;
+    }
+
+    public void setNome(String nome){
+        nome = verificaTamanho(nome, 30);
+        pesssoa.setNome(nome.toUpperCase().toUpperCase());
+    }
+
+    private String verificaTamanho(String valor, int tamanhoFinal) {
+        if(valor.length() < tamanhoFinal){
+            return adicionalEspaco(valor, tamanhoFinal);
+        }else if (valor.length() > tamanhoFinal) {
+            return valor.substring(0,tamanhoFinal);
+        }else {
+            return valor;
+        }
+    }
+
+    public void setCpf(String cpf) throws CadastroException {
+        cpf = cpf.replaceAll("[^0-9]+","");
+        if (cpf.length() == 11) {
+            pesssoa.setCpf(cpf);
+        } else {
+            throw new CadastroException("CPF inválido!");
+        }
+    }
+
+    public void setDataNascimento(String dataNascimento) throws CadastroException{
+        if (dataNascimento.length() == 10) {
+            pesssoa.setDataNascimento(LocalDate.parse(dataNascimento, fmt));
+        } else {
+            throw new CadastroException("Data inválida: digite uma data assim dd/MM/YYYY");
+        }
+    }
+
+    public void setSexo(Character sexo) throws CadastroException{
+        if (sexo == 'M') {
+            pesssoa.setSexo(SEXO.MASCULINO);
+        } else if (sexo == 'M'){
+            pesssoa.setSexo(SEXO.FEMiNINO);
+        }else {
+            throw new CadastroException("Sexo inválido: digite M para Masculino ou F para Feminino");
+        }
+    }
+
+    public void setLogradouro(String logradouro){
+        logradouro = verificaTamanho(logradouro, 40);
+        pesssoa.getEndereco().setLogradouro(logradouro.toUpperCase());
+    }
+
+    private String adicionalEspaco(String valor, int tamanhoFinal ) {
+        StringBuilder espaco = new StringBuilder();
+        for (int i = valor.length(); i<tamanhoFinal; i++){
+            espaco.append(" ");
+        }
+        espaco.append(valor);
+        return espaco.toString();
+    }
+
+    public void setNumero(String numero){
+        if (numero.length() < 5) {
+            numero = preecherZero(numero,5);
+            pesssoa.getEndereco().setNumero(numero);
+        } else {
+            pesssoa.getEndereco().setNumero(numero);
+        }
+    }
+
+    private String preecherZero(String numero, int tamanhoFinal) {
+        StringBuilder zero = new StringBuilder();
+        for (int i = numero.length(); i<tamanhoFinal; i++){
+            zero.append("0");
+        }
+        zero.append(numero);
+        return zero.toString();
+    }
+
+    public void setComplemento(String complemento){
+        complemento = verificaTamanho(complemento, 20);
+        pesssoa.getEndereco().setComplemento(complemento.toUpperCase());
+    }
+
+    public void setBairro(String bairro){
+        bairro = verificaTamanho(bairro, 30);
+        pesssoa.getEndereco().setBairro(bairro.toUpperCase());
+    }
+
+    public void setCidade(String cidade){
+        cidade = verificaTamanho(cidade, 40);
+        pesssoa.getEndereco().setCidade(cidade.toUpperCase());
+    }
+
+    public void setEstado(String estado){
+        pesssoa.getEndereco().setEstado(estado.toUpperCase());
+    }
+
+    public void setEmail(String email){
+        email = verificaTamanho(email, 50);
+        pesssoa.getContato().setEmail(email.toLowerCase());
+    }
+
+    public void setTelefone(String telefone){
+        telefone = verificaNumero(telefone);
+        pesssoa.getContato().setTelefone(telefone);
+    }
+
+    public void setCelular(String celular){
+        celular = verificaNumero(celular);
+        pesssoa.getContato().setCelular(celular);
+    }
+
+    private String verificaNumero(String numero) {
+        numero.replaceAll("[^0-9]","");
+        if (numero.length() != 0) {
+            return numero;
+        } else {
+            return preecherZero(numero, 11);
+        }
+    }
+
+    public void setWhatsApp(Character whatsApp){
+        if (whatsApp == 'T') {
+            pesssoa.getContato().setWhatsApp(true);
+        }else {
+            pesssoa.getContato().setWhatsApp(false);
+        }
+    }
+
+    public void setCargo(String cargo) {
+        cargo = verificaTamanho(cargo, 30);
+        pesssoa.getProfissao().setCargo(cargo.toUpperCase());
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa){
+        nomeEmpresa = verificaTamanho(nomeEmpresa,30);
+        pesssoa.getProfissao().setNomeEmpresa(nomeEmpresa.toUpperCase());
+    }
+
+    public void setSalario(String salario){
+        String sal = salario.replaceAll("[^0-9,]","").replace(",",".");
+        pesssoa.getProfissao().setSalario(Double.valueOf(sal));
+    }
+
+    public void setEmpregoAtual(Character empregoAtual){
+        if (empregoAtual == 'T') {
+            pesssoa.getProfissao().setEmpregoAtual(true);
+        }else {
+            pesssoa.getProfissao().setEmpregoAtual(false);
+        }
+    }
+
+    public void setPretencaoMinima(String salario){
+        String sal = salario.replaceAll("[^0-9,]","").replace(",",".");
+        pesssoa.getPretencaoSalarial().setValorMinimo(Double.valueOf(sal));
+    }
+
+    public void setPretencaoMaxima(String salario){
+        String sal = salario.replaceAll("[^0-9,]","").replace(",",".");
+        pesssoa.getPretencaoSalarial().setValorMaximo(Double.valueOf(sal));
+    }
+
+    public void setHabilidades(String habilidades){
+        String[] listaHabilidade = habilidades.replace(" ","").split(",");
+
+        for (String nomeHabilidade : listaHabilidade) {
+            Habilidade habilidade = new Habilidade();
+            habilidade.setNome(verificaTamanho(nomeHabilidade,50));
+            pesssoa.getHabilidades().add(habilidade);
+        }
+    }
+}
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,31 +1,43 @@
-import model.Cadastro;
+package view;
+
+import controller.Cadastro;
+import exception.CadastroException;
 import model.Habilidade;
 
 public class Main {
     public static void main(String[] args) {
-        Cadastro cadastro = new Cadastro();
-        cadastro.setNome("Raimundo Nonato Loureiro Castelo Branco");
-        cadastro.setCpf("135.217.791-18");
-        cadastro.setDataNascimento("16/04/1986");
-        cadastro.setSexo('F');
-        cadastro.setLogradouro("Ruas das laranjeiras");
-        cadastro.setNumero("123");
-        cadastro.setComplemento("Bl 03 ap 113");
-        cadastro.setBairro("Vila Nazare");
-        cadastro.setCidade("Teresina");
-        cadastro.setEstado("Pi");
-        cadastro.setEmail("raimundo.nonato@gmail.com");
-        cadastro.setTelefone("(11) 3351-1010");
-        cadastro.setCelular("(11) 94565-2353");
-        cadastro.setWhatsApp('T');
-        cadastro.setCargo("Analista de sistemas");
-        cadastro.setNomeEmpresa("Microsoft");
-        cadastro.setNomeEmpresa("Microsoft");
-        cadastro.setSalario("R$ 5.279,33");
-        cadastro.setEmpregoAtual('F');
-        cadastro.setPretencaoMinima("R$ 5.279,33");
-        cadastro.setPretencaoMaxima("R$ 5.279,33");
-        cadastro.setHabilidades("Java, Vue3, Postgres, Spring");
+        /* TODO
+                criar mais clsse
+                dados null
+         */
+        Cadastro cadastro = null;
+        try {
+            cadastro = new Cadastro();
+            cadastro.setNome("Raimundo Nonato Loureiro Castelo Branco");
+            cadastro.setCpf("135.217.791-18");
+            cadastro.setDataNascimento("16/04/1986");
+            cadastro.setSexo('M');
+            cadastro.setLogradouro("Ruas das laranjeiras");
+            cadastro.setNumero("123");
+            cadastro.setComplemento("Bl 03 ap 113");
+            cadastro.setBairro("Vila Nazare");
+            cadastro.setCidade("Teresina");
+            cadastro.setEstado("Pi");
+            cadastro.setEmail("raimundo.nonato@gmail.com");
+            cadastro.setTelefone("(11) 3351-1010");
+            cadastro.setCelular("(11) 94565-2353");
+            cadastro.setWhatsApp('T');
+            cadastro.setCargo("Analista de sistemas");
+            cadastro.setNomeEmpresa("Microsoft");
+            cadastro.setNomeEmpresa("Microsoft");
+            cadastro.setSalario("R$ 5.279,33");
+            cadastro.setEmpregoAtual('F');
+            cadastro.setPretencaoMinima("R$ 5.279,33");
+            cadastro.setPretencaoMaxima("R$ 5.279,33");
+            cadastro.setHabilidades("Java, Vue3, Postgres, Spring");
+        } catch (CadastroException c) {
+            System.out.println(c.getMessage());
+        }
 
         System.out.println(cadastro.getPesssoa().getNome());
         System.out.println(cadastro.getPesssoa().getCpf());
